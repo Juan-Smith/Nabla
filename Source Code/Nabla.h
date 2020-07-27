3827dc49fb1c0e7f97acd4b0714dd135ada4c4c7
Juan Smith <68098265+Juan-Smith@users.noreply.github.com>
2020-07-27 23:47:27 +0200

    Added main and Nabla
    Added Driver program for the Nabla class as well as the Nabla class.

---
+++ Source Code/Nabla.h
@@ -0,0 +1,37 @@
+class ActivationFunction
+{
+
+};
+
+class Neuron
+{
+    public:
+    Neuron() {}
+};
+
+class Layer
+{
+    private : 
+    Neuron myneuron;
+    public :
+    Layer() { }
+};
+
+class NeuralNetwork
+{
+    //public:
+  //   Layer layer;
+  //   Neuron neuron;
+  //  void CreateNeuralNetwork(int*, int*, int);
+  //  void DestroyNeuralNetwork() const;
+};
+
+/*void NeuralNetwork::CreateNeuralNetwork(int* layer_sizes, int* activation_functions, int num_layers)
+{
+
+}
+
+void NeuralNetwork::DestroyNeuralNetwork() const
+{
+
+} */
---
+++ Source Code/main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include "Nabla.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "Hallo World";
+    NeuralNetwork mynetwork;
+    Layer mylayer;
+    Neuron myneuron;
+    return 0;
+}
